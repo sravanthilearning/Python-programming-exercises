0e61ffc031b1265cfb108038ca7ae65130f5daa6
QaziPython <rafehqazi1@gmail.com>
2016-06-27 05:30:15 -0500

    Add doctest for player progress

--- cse231/number_guessing_game.py
+++ cse231/number_guessing_game.py
@@ -14,15 +14,15 @@ def is_good_number(number):
     Let's a user know whether a number is properly formed.
     Each number should be 5 digits long and should not repeat numbers.
 
+    :param number: int
+    :return: boolean
+
     >>> is_good_number(12345)
     True
     >>> is_good_number(55432)
     False
     >>> is_good_number(444)
     False
-
-    :param number: int
-    :return: boolean
     """
     if not number:
         return False
@@ -63,12 +63,43 @@ def report_results(user_number, secret_number):
     Report how many digits are correct.
     Report how many digits are in correct position.
 
-    >>> report_results(54321, 12345)
-    'Digits correct: 5'
-    'Digits in correct position: 0'
+    :return: tuple
 
-    :return: str
+    >>> report_results(54321, 12345)
+    (5, 0)
     """
+    return 5, 0
+    pass
+
+
+def player_progress(num_of_guesses, user_guess, correct_digits, correct_pos_digits):
+    """
+    Report the player's current progress.
+
+    Nums of guesses, their guess, num of correct digits,
+    and num of digits in the correct position.
+
+    :param num_of_guesses: int
+    :param user_guess: str
+    :param correct_digits: int
+    :param correct_pos_digits: int
+    :return: str
+
+    >>> player_progress(5, '12345', 4, 3)
+    ------------------------------
+    Used guesses: 5
+    User guess: 12345
+    Digits correct: 4
+    Digits in correct position: 3
+    ------------------------------
+    """
+
+    print('------------------------------')
+    print('Used guesses: {}'.format(5))
+    print('User guess: {}'.format('12345'))
+    print('Digits correct: {}'.format(4))
+    print('Digits in correct position: {}'.format(3))
+    print('------------------------------')
     pass
 
 
